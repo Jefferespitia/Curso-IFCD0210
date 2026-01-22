2d72a42b2ff41bfd2b186134e6a1ebb4b4ce1680
Jefferson Espitia <Jefferespitia@gmail.com>
2026-01-22 13:56:29 +0100

    update second part

---
+++ Demo-js/exercises/ex14.js
@@ -0,0 +1,15 @@
+// 15. Crea una función que reciba un número por parámetros e imprima por consola si el número recibido es un número primo.
+
+function isPrime(n) {
+    let result
+    return result 
+for (let i = 2; i < n; i++) {
+   if (n%i === 0) 
+    result = false
+    break  
+ }
+return result
+
+con
+
+}
